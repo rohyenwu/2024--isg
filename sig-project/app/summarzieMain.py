592241f653c409717bd13ffbbac35d234c378b0f
TjUsername <qoxowls123@gmail.com>
2024-07-27 19:30:05 +0900

    키워드 찾기 업데이트

--- sig-project/app/summarzieMain.py
+++ sig-project/app/summarzieMain.py
@@ -9,7 +9,8 @@ mydb=get_db_connection
 cursor=mydb.cursor()
 data = fetch_reviews(cursor)
 
-
+# 키워드를 찾아서 데이터베이스에 올린다.
+keywords_by_category = extract_similar_keywords_by_category(data, model, top_n=10)
 
 #요약한걸 데이터베이스에 올린다.               
 store_summaries(cursor, reviews_by_game_and_category)
