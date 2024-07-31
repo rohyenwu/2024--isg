ba53e452f8206bb56da19920c63e7f5a31e99ff7
iusong <wsong22@naver.com>
2024-07-31 16:25:13 +0900

    find_similar_keywords 수정

--- sig-project/preprocessing/summarize.py
+++ sig-project/preprocessing/summarize.py
@@ -1,6 +1,5 @@
-import mysql.connector
+import torch
 import fasttext
-from collections import defaultdict
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 
 def load_FestText():
@@ -21,11 +20,12 @@ def load_Pegasus():
 # FastText 모델을 사용하여 유사한 단어를 찾는 함수
 def find_similar_keywords(model, keyword, top_n):
     similar_words = []
-    extracted_words = [keyword] 
+    extracted_words = [keyword]
 
     # 유사 단어 추출
     similar = model.get_nearest_neighbors(keyword)
     for sim_word, _ in similar:
+        sim_word = str(sim_word) #문자열로 변환하도록 수정
         if not contains_substring(extracted_words, sim_word):
             similar_words.append(sim_word)
             extracted_words.append(sim_word)
@@ -59,15 +59,14 @@ def find_sentence(text, word):
 
     # 문장을 마침표, 느낌표 같은 문장 구분기호를 기준으로 나눔
     sentences = sent_tokenize(text)
-    
+
     for sentence in sentences:
         # 문장에 word가 있으면 result_sentence에 넣기
         if word in sentence.lower():
             result_sentence.append(sentence)
-    
+
     result = ' '.join(result_sentence)
     return result
 
 # #키워드가 포함된 코퍼스들을 훈련된 모델로 요약하는 함수
 # def summrize_by_Pegasus(keywords):
-
