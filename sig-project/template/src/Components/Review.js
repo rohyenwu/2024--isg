b83d8ccbd0b304ebaafa199a345ce55b5b91f735
TjUsername <qoxowls123@gmail.com>
2024-08-10 23:36:14 +0900

    UI update

--- sig-project/template/src/Components/Review.js
+++ sig-project/template/src/Components/Review.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './Review.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook, faVolumeUp, faDesktop, faLightbulb } from "@fortawesome/free-solid-svg-icons";
 
 function ReviewPage() {
   const location = useLocation();
@@ -9,10 +11,10 @@ function ReviewPage() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   const query = new URLSearchParams(location.search).get('query');
 
-  // URL 쿼리 파라미터가 변경되면 해당 쿼리로 데이터를 fetch합니다
   useEffect(() => {
     if (query) {
       setSearchTerm(query);
@@ -34,15 +36,14 @@ function ReviewPage() {
         return response.json();
       })
       .then(data => {
-        if(data)
+        if (data) {
           setData(data);
-        else
-          alert('게임이 존재하지 않습니다.');
+        } else {
+        }
         setLoading(false);
       })
       .catch(error => {
         setError(error);
-        
         alert('오류');
         setLoading(false);
       });
@@ -51,67 +52,99 @@ function ReviewPage() {
   const handleSearch = (event) => {
     event.preventDefault();
     if (searchTerm.trim()) {
-      // URL 쿼리 파라미터 업데이트
       navigate(`?query=${encodeURIComponent(searchTerm.trim())}`);
     }
   };
 
-  // 로딩 중일 때
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
 
   return (
     <div>
-      <div className="search-container">
-        <form id="searchForm" onSubmit={handleSearch}>
-          <input
-            type="text"
-            name="gamename"
-            id="gamename"
-            placeholder="게임 이름"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <input type="submit" value="Search" />
-        </form>
-        <div className="game-name" id="gameName">
-          {query || '게임 이름을 입력하세요'}
+      <div className="header">
+        <div className='projectName'>
+          게임 리뷰 탐색기
+        </div>
+        <div className="keywords-container">
+          <button className="keyword-button" onClick={() => handleCategoryChange('graphic')}><FontAwesomeIcon icon={faDesktop} />  그래픽</button>
+          <button className="keyword-button" onClick={() => handleCategoryChange('sound')}><FontAwesomeIcon icon={faVolumeUp} />  사운드</button>
+          <button className="keyword-button" onClick={() => handleCategoryChange('story')}><FontAwesomeIcon icon={faBook} />  스토리</button>
+          <button className="keyword-button" onClick={() => handleCategoryChange('creativity')}><FontAwesomeIcon icon={faLightbulb} />  창의성</button>
+        </div>
+
+        <div className="search-container">
+          <form id="searchForm" onSubmit={handleSearch}>
+            <input
+              type="text"
+              name="gamename"
+              id="gamename"
+              placeholder="게임 이름"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <input type="submit" value="Search" />
+          </form>
         </div>
       </div>
-      <div className="evaluation-container">
-        <div>
-          <label>그래픽</label>
-          <div className="review" id="graphicReview">{data?.graphic || '그래픽 긍정 리뷰'}</div>
+
+      <div className="main-content">
+        <div className="game-name" id="gameName">
+          {query}
         </div>
-        <div>
-          <label>사운드</label>
-          <div className="review" id="soundReview">{data?.sound || '사운드 긍정 리뷰'}</div>
-        </div>
-        <div>
-          <label>스토리</label>
-          <div className="review" id="storyReview">{data?.story || '스토리 긍정 리뷰'}</div>
-        </div>
-        <div>
-          <label>창의성</label>
-          <div className="review" id="creativityReview">{data?.creativity || '창의성 긍정 리뷰'}</div>
-        </div>
-        <div className="new-row"></div>
-        <div>
-          <div className="review" id="graphicNativeReview">{data?.graphicNative || '그래픽 부정 리뷰'}</div>
-          <div className="score" id="graphicScore">{data?.graphicScore}</div>
-        </div>
-        <div>
-          <div className="review" id="soundNativeReview">{data?.soundNative || '사운드 부정 리뷰'}</div>
-          <div className="score" id="soundScore">{data?.soundScore}</div>
-        </div>
-        <div>
-          <div className="review" id="storyNativeReview">{data?.storyNative || '스토리 부정 리뷰'}</div>
-          <div className="score" id="storyScore">{data?.storyScore}</div>
-        </div>
-        <div>
-          <div className="review" id="creativityNativeReview">{data?.creativityNative || '창의성 부정 리뷰'}</div>
-          <div className="score" id="creativityScore">{data?.creativityScore}</div>
+
+        <div className="evaluation-container">
+          {selectedCategory === 'graphic' && (
+            <>
+              <div className="review">
+                <div className='review-positiveheader'>긍정적리뷰<hr></hr></div>
+                {data?.graphic || '그래픽 긍정 리뷰'}
+              </div>
+              <div className="review">
+                <div className='review-negativeheader'>부정적리뷰<hr></hr></div>
+                {data?.graphicNative || '그래픽 부정 리뷰'}
+              </div>
+            </>
+          )}
+
+          {selectedCategory === 'sound' && (
+            <>
+              <div className="review">
+                <div className='review-positiveheader'>긍정적리뷰<hr></hr></div>
+                {data?.sound || '사운드 긍정 리뷰'}
+              </div>
+              <div className="review">
+                <div className='review-negativeheader'>부정적리뷰<hr></hr></div>
+                {data?.soundNative || '사운드 부정 리뷰'}
+              </div>
+            </>
+          )}
+
+          {selectedCategory === 'story' && (
+            <>
+              <div className="review">
+                <div className='review-positiveheader'>긍정적리뷰<hr></hr></div>
+                {data?.story || '스토리 긍정 리뷰'}
+              </div>
+              <div className="review">
+                <div className='review-negativeheader'>부정적리뷰<hr></hr></div>
+                {data?.storyNative || '스토리 부정 리뷰'}
+              </div>
+            </>
+          )}
+
+          {selectedCategory === 'creativity' && (
+            <>
+              <div className="review">
+                <div className='review-positiveheader'>긍정적리뷰<hr></hr></div>
+                {data?.creativity || '창의성 긍정 리뷰'}
+              </div>
+              <div className="review">
+                <div className='review-negativeheader'>부정적리뷰<hr></hr></div>
+                {data?.creativityNative || '창의성 부정 리뷰'}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
